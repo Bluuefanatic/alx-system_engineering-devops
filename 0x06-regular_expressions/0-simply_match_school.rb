4bf9a65db2ff6f660e7438e8d7d30d11d8c299d2
Bluuefanatic <izienjoel@gmail.com>
2024-01-30 15:46:33 -0800

    Add Ruby script for matching input against 'School' regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school?(input)
-  regex = /School/
-  input =~ regex
-end
-
-if ARGV.length != 1
-  puts "Usage: ruby script.rb <input>"
-  exit 1
-end
-
-input = ARGV[0]
-
-if match_school?(input)
-  puts "#{input} matches the regular expression 'School'."
-else
-  puts "#{input} does not match the regular expression 'School'."
-end
+puts ARGV[0].scan(/School/).join
