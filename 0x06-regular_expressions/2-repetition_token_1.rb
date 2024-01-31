b1acf83c91b807366e1001cafa6bcef2e32cf7af
Bluuefanatic <izienjoel@gmail.com>
2024-01-30 16:02:16 -0800

    Create Ruby script to match 'School' in input using a regular expression

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school?(input)
-  regex = /\bSchool\b/i  # Case-insensitive match for the whole word
-  !!(input =~ regex)
-end
-
-if ARGV.length != 1
-  puts "Usage: ruby script.rb <input>"
-  exit 1
-end
-
-input = ARGV[0]
-
-if match_school?(input)
-  puts "#{input} contains the word 'School'."
-else
-  puts "#{input} does not contain the word 'School'."
-end
+puts ARGV[0].scan(/hb?t?n/).join
